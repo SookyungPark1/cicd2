e647c90121af5e36390c2cf93229fd313457a7f4
sookyung <sookyung963@naver.com>
2025-06-12 16:37:28 +0900

    cicd2

---
+++ src/main/java/com/example/cicd_acorn_server2/CicdAcornServer2Application.java
@@ -0,0 +1,13 @@
+package com.example.cicd_acorn_server2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdAcornServer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdAcornServer2Application.class, args);
+	}
+
+}
